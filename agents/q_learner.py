Fixed issue in whitelist learner training. Fixed reward calculation in VaseWorld.

agents/q_learner.py
@@ -14,6 +14,7 @@ class QLearner:
         self.Q = np.zeros((simulator.height, simulator.width, len(self.actions)))  # TODO check dimensionality
         self.greedy_a, self.greedy_v = np.ones((simulator.height, simulator.height), int), \
                                        np.zeros((simulator.height, simulator.height))  # greedy record-keeping
+        self.greedy_v.fill(float('-inf'))
         self.num_samples = np.zeros((simulator.height, simulator.width, len(self.actions)), int)
 
         self.train(simulator)  # let's get to work!
@@ -24,25 +25,19 @@ class QLearner:
             row, col = randint(0, simulator.height - 1), randint(0, simulator.width - 1)
 
             # Choose according to explore/exploit
-            if random() < self.epsilon:
-                action = randint(0, len(self.actions) - 1)
-                while action == self.greedy_a[row][col]:  # make sure we don't choose greedy action
-                    action = randint(0, len(self.actions) - 1)
-            else:
-                action = self.greedy_a[row][col]
+            action = self.e_greedy_action(row, col)
 
             # Update sample count and learning rate
             self.num_samples[row][col][action] += 1
             learning_rate = 1 / self.num_samples[row][col][action]
 
             # Go to new simulator state and take action
-            simulator.reset()  # TODO how do we track distance to goal?
             simulator.set_agent_pos(simulator.agent_pos, (row, col))
             simulator.agent_pos = [row, col]
 
-            reward = simulator.get_reward()
             simulator.take_action(self.actions[action])
             new_state = simulator.agent_pos
+            reward = simulator.get_reward()
 
             # Perform TD update
             self.Q[row][col][action] += learning_rate * (reward + self.discount * max(self.Q[new_state[0]][new_state[1]])
@@ -51,6 +46,15 @@ class QLearner:
             # See if this is better than state's current greedy action
             if self.Q[row][col][action] > self.greedy_v[row][col]:
                 self.greedy_a[row][col], self.greedy_v[row][col] = action, self.Q[row][col][action]
+
+    def e_greedy_action(self, row, col):
+        """Returns the e-greedy action index for the given row and column."""
+        action = self.greedy_a[row][col]
+        if random() < self.epsilon:
+            action = randint(0, len(self.actions) - 1)
+            while action == self.greedy_a[row][col]:  # make sure we don't choose greedy action
+                action = randint(0, len(self.actions) - 1)
+        return action
 
     def choose_action(self, state):
         return self.actions[self.greedy_a[state.agent_pos[0]][state.agent_pos[1]]]